d34e216427483a0ec1a2e94aaff72ec857905513
Muhammad-Yuda-Yudistira <118842119+Muhammad-Yuda-Yudistira@users.noreply.github.com>
2023-04-07 02:21:40 +0700

    animation hujan is done, design again web and cursor, add donation, add footer

--- js/script.js
+++ js/script.js
@@ -1,27 +1,15 @@
-const jumHujan = 10;
+const jumHujan = 4;
 const container = document.querySelector('.container')
 
-for(let i = 0; jumHujan; i++) {
+for(let i = 1; i <= jumHujan; i++) {
     const hujan = document.createElement('span')
     hujan.classList.add('hujan')
 
     const butir = Math.floor(Math.random() * 100 + 1)
-    hujan.style.transform = "rotate(45deg) scale(" + butir * 2 +"%)"
+    hujan.style.transform = "rotate(45deg) scale(" + butir * 2 + "%)"
     hujan.style.left = butir + "%"
-    hujan.style.animationDuration = butir / 10 + "s"
+    hujan.style.animationDuration = butir / 3 + "s"
     hujan.style.animationDelay = butir / 10 + "s"
 
     container.appendChild(hujan)
 }
-
-
-// const hujan = document.querySelectorAll('.hujan')
-
-// for(let i = 0; i < hujan.length; i++) {
-//     const butir = Math.floor(Math.random() * 100 + 1)
-//     hujan[i].style.transform = "rotate(45deg) scale(" + butir * 2 +"%)"
-//     hujan[i].style.left = butir + "%"
-//     hujan[i].style.animationDuration = butir / 10 + "s"
-//     hujan[i].style.animationDelay = butir / 10 + "s"
-//     console.log(butir)
-// }
